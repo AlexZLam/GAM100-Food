f397a37f4d4939ca327f0dbc8923d8e20f138a7b
Alex Lam <alexzlam@outlook.com>
2025-10-15 07:54:06 -0700

    Baking finished?

--- GAM100 Food/Assets/Scripts/baking.cs
+++ GAM100 Food/Assets/Scripts/baking.cs
@@ -5,43 +5,67 @@ public class Baking : MonoBehaviour
 {
     public camera_move camera_Move;
     public GameObject slider;
-    private float time = 5.0f;
-    private float countdown;
-   
+    public GameObject baking;
+
     [SerializeField]
     private Button button;
+    [SerializeField]
+    private RawImage dark;
 
+    private const float time = 5.0f;
+    private const float successThreshold = 0.9f;
+
+    private float countdown;
     private bool isCountingDown = false;
+    private float alpha;
+
+    private Color newColor;
+    private CanvasGroup sliderGroup;
+    private Slider sliderComponent;
+ 
 
     void Start()
     {
+        sliderComponent = slider.GetComponent<Slider>();
+        sliderGroup = slider.GetComponent<CanvasGroup>();
 
-        slider.GetComponent<Slider>().maxValue = 5;
-        slider.GetComponent<Slider>().value = countdown;
-        slider.SetActive(false);
-        button.enabled = false;
-        slider.GetComponent<Slider>().enabled = false;
-        button.GetComponent<Image>().enabled = false;
+        newColor = dark.color;
+        newColor.a = 0;
+
+        sliderComponent.maxValue = time;
         countdown = time;
+        sliderComponent.value = countdown;
+
         button.onClick.AddListener(OnButtonClick);
     }
-    
+
     void Update()
     {
-        slider.GetComponent<Slider>().value = countdown;
-
-        setBakingActive();
-            
         if (isCountingDown)
         {
             Timer();
         }
-        //Debug.Log(countdown);
+
+        alpha = Mathf.Clamp01(1.0f - Mathf.Pow(countdown / time, 7));
+        sliderComponent.value = countdown;
+
+        setBakingActive();
     }
 
     private void Timer()
     {
         countdown -= Time.deltaTime;
+
+        // Update dark overlay alpha
+        newColor.a = alpha;
+        dark.color = newColor;
+
+        // Fade out slider
+        if (sliderGroup != null)
+        {
+            sliderGroup.alpha = 1.0f - alpha;
+        }
+
         if (countdown <= 0f)
         {
             isCountingDown = false;
@@ -54,44 +78,27 @@ public class Baking : MonoBehaviour
         countdown = time;
         isCountingDown = true;
         Debug.Log("Button clicked, countdown started.");
-        float sliderValue = slider.GetComponent<Slider>().value;
 
-        // Check if slider is between 0.5 and 2.0
-        if (sliderValue >= 0.5f && sliderValue <= 2.0f)
+        float sliderValue = sliderComponent.value;
+
+        if (sliderValue > 0 && sliderValue <= successThreshold)
         {
             isCountingDown = false;
-            button.enabled = false;
 
-            Debug.Log("Button clicked while slider is between 0.5 and 2.0!");
-            // You can add any special behavior here
-        }       
+            Finished();
+
+            Debug.Log("Button clicked while slider is between 0 and 0.9!");
+        }
     }
 
     private void setBakingActive()
     {
-        if (camera_Move.current_game == camera_Move.baking)
-        {
-            button.enabled = true;
-            button.GetComponent<Image>().enabled = true;
-        }
-
-        else
-        {
-            button.enabled = false;
-            button.GetComponent<Image>().enabled = false;
-        }
-        if (camera_Move.current_game == camera_Move.baking)
-        {
-            slider.SetActive(true);
-            slider.GetComponent<Slider>().enabled = true;
-        }
-
-        else
-        {
-            slider.SetActive(false);
-            slider.GetComponent<Slider>().enabled = false;
-        }
+        baking.SetActive(camera_Move.current_game == camera_Move.baking);
     }
-   
 
+    public void Finished()
+    {
+        sliderGroup.alpha = 1;
+        dark.color = new Color(newColor.r, newColor.g, newColor.b, 0);
+    }
 }
