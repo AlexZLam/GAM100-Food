55c6abb0891eb8d8b270d4194c580c79d65ce255
Alex Lam <alexzlam@outlook.com>
2025-10-28 07:38:36 -0700

    Update HomeStation.cs

--- GAM100 Food/Assets/Scripts/HomeStation.cs
+++ GAM100 Food/Assets/Scripts/HomeStation.cs
@@ -1,7 +1,12 @@
 using UnityEngine;
+using static UnityEditor.SceneView;
 
 public class HomeStation : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject home;
+
+    public camera_move camera_Move;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +16,10 @@ public class HomeStation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        setHomeActive();
+    }
+    private void setHomeActive()
+    {
+        home.SetActive(camera_Move.current_game == camera_Move.home);
     }
 }
